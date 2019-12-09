009853806be89e5ae8c3f4267988e89596e4f808
Paul Newman <52367352+ostechs@users.noreply.github.com>
2019-12-09 08:58:08 -0700

    Add files via upload

---
+++ myjava.js
@@ -0,0 +1,5 @@
+document.getElementById("lastMod").innerHTML = document.lastModified;
+
+function myAlert() {
+    alert("Thank you for contacting Cocina Siciliana. We will be contacting you shortly.")
+}
